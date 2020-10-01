ee2657c1e3dd25644fa25cf9a40059c7a61a4cf2
emanuele5180 <macina5180>
2020-10-01 18:33:24 +0200

    model + protected fillable

---
+++ app/Employee.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+  protected $fillable = [
+
+
+   'firstname',
+   'lastname',
+   'date_of_birth',
+   'private_code',
+   'location_id'
+
+
+  ];
+}
