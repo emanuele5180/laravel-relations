2082ff388f78863092309a9cf1bd1a469bb0966e
emanuele5180 <macina5180>
2020-10-05 02:24:46 +0200

    ok

--- resources/views/emp-show.blade.php
+++ resources/views/emp-show.blade.php
@@ -11,23 +11,17 @@
 
   <ul>
     <li>
-
       date of birth: {{ $emp -> date_of_birth }}
-
     </li>
+
     <li>
-
       private code: {{ $emp -> private_code }}
-
     </li>
+
     <li>
       Location: {{ $emp -> location -> name }}
-      (
         ({{ $emp -> location -> city }} -
         {{ $emp -> location -> state }})
-
-
-      )
     </li>
 
 
